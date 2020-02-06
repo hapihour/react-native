de2c4cf5af650842e85ab9e63fff58b77ef29089
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-02-06 23:01:36 +1100

    Extracted App content to main and added PaperProvider and NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import Main from './src/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
