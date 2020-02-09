e2b0f234b3ab0eb70f7c40b4ffec3402f0709e70
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-02-09 14:21:13 +1100

    Updated config.example.ts

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,4 +1,29 @@
-export const firebaseConfig: Object = {
+type FirebaseConfig = {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
+
+type GoogleConfig = {
+  apiKey: string;
+};
+
+type ApiConfig = {
+  apiHost: string;
+};
+
+type AlgoliaConfig = {
+  appId: string;
+  clientApiKey: string;
+  userIndex: string;
+  eventsIndex: string;
+};
+
+export const firebaseConfig: FirebaseConfig = {
   apiKey: "",
   authDomain: "",
   databaseURL: "",
@@ -6,4 +31,19 @@ export const firebaseConfig: Object = {
   storageBucket: "",
   messagingSenderId: "",
   appId: ""
+};
+
+export const googleConfig: GoogleConfig = {
+  apiKey: ""
+};
+
+export const apiConfig: ApiConfig = {
+  apiHost: ""
+};
+
+export const algoliaConfig: AlgoliaConfig = {
+  appId: "",
+  clientApiKey: "",
+  userIndex: "",
+  eventsIndex: ""
 };
