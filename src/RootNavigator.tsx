9bd8d5004f1da30e4ebf50e6641c406668a0a592
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-02-09 16:00:15 +1100

    Added push notification

--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { DefaultTheme, DarkTheme } from "@react-navigation/native";
 import { useTheme } from "react-native-paper";
 import { DrawerContent } from "./components/DrawerContent";
 import { StackNavigator } from "./stack";
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import {setUserPushToken} from "./actions/auth";
+import {AuthContext} from "./context/Auth";
 
 const Drawer = createDrawerNavigator();
 
 export const RootNavigator = () => {
   const theme = useTheme();
   const navigationTheme = theme.dark ? DarkTheme : DefaultTheme;
+  const { fetchToken } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+      if (status !== 'granted') {
+        return;
+      }
+
+      const pushToken = await Notifications.getExpoPushTokenAsync();
+      await setUserPushToken(await fetchToken(), pushToken);
+    })();
+  }, []);
+
 
   return (
     <NavigationContainer theme={navigationTheme}>
---
+++ src/actions/auth.ts
@@ -0,0 +1,22 @@
+import {apiConfig} from "../config";
+import {buildHeaders} from "./helper";
+
+const { apiHost } = apiConfig;
+
+export const setUserPushToken = async (token: string, pushToken: string): Promise<void> => {
+  const url = `${apiHost}/user-push-token`;
+  console.log(url);
+  console.log(token);
+
+  const res = await fetch(url, {
+    headers: buildHeaders(token),
+    method: "POST",
+    body: JSON.stringify({
+      pushToken
+    })
+  });
+
+  if (!res.ok) {
+    throw new Error("Unable to save push token");
+  }
+}
